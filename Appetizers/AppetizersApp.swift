458838edfdfa101e5e2134de1143e7f84ae98b30
Ivan Trajanovski <ivantrajanovski@gmail.com>
2024-02-21 20:14:03 +0100

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Ivan Trajanovski on 21.02.24.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
