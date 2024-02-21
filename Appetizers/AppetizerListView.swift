abff2972f7db1e9a8c8ac47776ae2f9bdc514a97
Ivan Trajanovski <ivantrajanovski@gmail.com>
2024-02-21 20:50:51 +0100

    create appetizer list view

--- Appetizers/AppetizerListView.swift
+++ Appetizers/AppetizerListView.swift
@@ -10,8 +10,10 @@ import SwiftUI
 struct AppetizerListView: View {
     var body: some View {
         NavigationView {
-            Text("Appetizers")
-                .navigationTitle("🫒Appetizers")
+            List(MockData.appetizers) { appetizer in
+                AppetizerListCell(appetizer: appetizer)
+            }
+            .navigationTitle("🫒Appetizers")
         }
     }
 }
